1b55ed173fd03d70784f84f85d6c25c070d73ef3
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2024-01-16 00:54:39 -0800

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'https://filesharebackend-29wu.onrender.com';
+const API_URI = 'https://fileshareingbackend.onrender.com';
 
 export const uploadFile = async (data) => {
     try {
